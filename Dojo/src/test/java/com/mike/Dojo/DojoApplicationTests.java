6646bbf20fbf6f2055c9f90a962634d550f45280
Michael Shin <Michaelshin.115@gmail.com>
2021-03-02 00:34:03 -0800

    first commit

---
+++ Dojo/src/test/java/com/mike/Dojo/DojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.Dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
